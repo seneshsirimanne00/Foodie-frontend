68a93f382e8d51b06c95d745dcb830ab5946a458
seneshsirimanne00 <seneshsirimanne@gmail.com>
2021-12-27 23:54:03 +0530

    frontend reupload

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { HeaderComponent } from './header/header.component';
+import { SectionOneComponent } from './landing-page/section-one/section-one.component';
+import { SectionTwoComponent } from './landing-page/section-two/section-two.component';
+import { SectionThreeComponent } from './landing-page/section-three/section-three.component';
+import { SectionFourComponent } from './landing-page/section-four/section-four.component';
+import { SectionFiveComponent } from './landing-page/section-five/section-five.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingPageComponent,
+    HeaderComponent,
+    SectionOneComponent,
+    SectionTwoComponent,
+    SectionThreeComponent,
+    SectionFourComponent,
+    SectionFiveComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
