3380e683073d7d926225e1b8e2c19675cf30c31f
seneshsirimanne00 <seneshsirimanne@gmail.com>
2022-01-02 23:59:02 +0530

    quiz part added

--- src/app/landing-page/section-four/section-four.component.ts
+++ src/app/landing-page/section-four/section-four.component.ts
@@ -5,11 +5,33 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './section-four.component.html',
   styleUrls: ['./section-four.component.css']
 })
-export class SectionFourComponent implements OnInit {
 
-  constructor() { }
+
+export class SectionFourComponent implements OnInit {
+  num = 0 ;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
+
+  onRightClick(){
+    if(this.num < 5){
+      this.num= this.num + 1;
+    }
+    console.log(this.num);
+  }
+
+  onLeftClick(){
+    if(this.num > 0){
+      this.num= this.num -1;
+    }
+  }
+
+  setNum(newNum :number){
+    this.num= newNum;
+  }
+
 
 }
